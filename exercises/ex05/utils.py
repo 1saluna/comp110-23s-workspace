45d441958716ffdd58de2bdcce49d5fb86f81b54
Isa Makhuli <isaluna@unc.edu>
2023-03-08 12:59:07 -0500

    ex05 fixed

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,7 +2,9 @@
 
 __author__ = "730572303"
 
-def only_evens(only_evens: list[int]) -> list[int]:
+
+def only_evens(only_evens: list[int]) -> list[int]:  # this makes a new list with only the even integers in the given list
+    """Making a new list with only the even integers."""
     result: list[int] = list()
     for index in range(len(only_evens)):
         if only_evens[index] % 2 == 0: 
@@ -10,12 +12,14 @@ def only_evens(only_evens: list[int]) -> list[int]:
     return result 
 
 
-def concat(l1: list[int], numbers: list[int]) -> list [int]:
+def concat(l1: list[int], numbers: list[int]) -> list[int]:  # this makes a new list with all of the numbers from both of the lists
+    """Combining the two lists to make a new big one."""
     new_list: list[int] = list(l1 + numbers)
     return new_list
 
 
-def sub(a_list: list[int], num_1: int, num_2: int) -> list[int]:
+def sub(a_list: list[int], num_1: int, num_2: int) -> list[int]:  # this makes a new list with integers in the range of indexes given
+    """Returning the indexes in the range."""
     new_list: list[int] = list()
     if num_1 < 0:
         num_1 = 0
@@ -24,5 +28,6 @@ def sub(a_list: list[int], num_1: int, num_2: int) -> list[int]:
     if len(a_list) == 0 or num_1 > len(a_list) or num_2 <= 0: 
         return list()
     for idx in range(num_1, num_2): 
+        num_2 = num_2 - 1
         new_list.append(a_list[idx])
     return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,50 +4,68 @@ __author__ = "730572303"
 
 from ex05.utils import only_evens, concat, sub
 
+
 def test_evens() -> None:
+    """List with only even numbers."""
     test_list: list[int] = [2, 4, 6]
     assert only_evens(test_list) == [2, 4, 6]
 
+
 def test_odds() -> None:
+    """List with only odd numbers."""
     test_list: list[int] = [3, 5, 7]
     assert only_evens(test_list) == []
 
+
 def test_empty_evens() -> None:
+    """List with no integers."""
     test_list: list[int] = []
     assert only_evens(test_list) == []
 
-#from exercises.ex05.utils import concat
+
+# from exercises.ex05.utils import concat
 
 def test_twolists() -> None:
+    """Combing two lists together."""
     l1: list[int] = [2, 4, 6]
     numbers: list[int] = [3, 2, 1]
     assert concat(l1, numbers) == [2, 4, 6, 3, 2, 1]
 
+
 def test_empty() -> None: 
+    """Combining two empty lists."""
     l1: list[int] = []
     numbers: list[int] = []
     assert concat(l1, numbers) == [] 
 
+
 def test_one_empty() -> None:
+    """Combining an empty list with a list of ints."""
     l1: list[int] = [4, 6, 7]
     numbers: list[int] = []
     assert concat(l1, numbers) == [4, 6, 7]
 
-#from exercises.ex05.utils import sub
+
+# from exercises.ex05.utils import sub
 
 def test_workingrange() -> None:
+    """Testing with a working range of ints."""
     a_list: list[int] = [10, 20, 30, 40]
     num_1: int = 1
     num_2: int = 3
     assert sub(a_list, num_1, num_2) == [20, 30]
 
+
 def test_emptylist() -> None:
+    """Testing with an empty list."""
     a_list: list[int] = []
     num_1: int = 1
     num_2: int = 3
     assert sub(a_list, num_1, num_2) == []
 
+
 def test_start_zero() -> None:
+    """Testing with a start index of a negative number to change to 0."""
     a_list: list[int] = [10, 20, 30, 40]
     num_1: int = -4
     num_2: int = 3
